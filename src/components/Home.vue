b156b423c0785aa9408a9f483868dddff4197636
VIVIFY <vivify@MacBook-Pro.local>
2020-01-22 13:33:34 +0100

    Add logout to user service

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,27 @@
 <template>
-<div class='container'>
-  <div id="app">
-      <button v-on:click='logout' class="btn btn-outline-dark btn-block text-uppercase register-btn">Logout</button>
-    <Todos :todos="todos" v-on:delete-todo="deleteTodo" v-on:update-todo.prevent='updateTodo'/>
-    <AddTodo v-on:add-todo="addTodo"/>
+  <div class="container">
+    <div id="app">
+      <button v-on:click="logout" class="btn btn-outline-dark btn-block text-uppercase">Logout</button>
+      <Todos :todos="todos" v-on:delete-todo="deleteTodo" v-on:update-todo.prevent="updateTodo" />
+      <AddTodo v-on:add-todo="addTodo" />
+    </div>
   </div>
-</div>
 </template>
 <script>
-
-import Todos from './Todos';
-import AddTodo from './AddTodo';
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+import { userService } from "../services/userServices";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Todos,
     AddTodo
   },
   data() {
     return {
-      todos: [],
-    }
+      todos: []
+    };
   },
   methods: {
     addTodo(newTodoObj) {
@@ -30,25 +30,15 @@ export default {
     deleteTodo(todoId) {
       this.todos = this.todos.filter(todo => todo.id !== todoId);
     },
-    updateTask(e, list){
+    updateTask(e, list) {
       list.title = e.target.innerText;
       e.target.blur();
     },
     logout() {
-        this.$http
-        .post("/logout")
-        .then(() => {
-            this.$router.replace(this.$route.query.redirect || "/login");
-            return 'Logged out';
-        })
-        .catch(() => {
-            return 'No!';
-        });
+      userService.logout();
     }
   }
-}
+};
 </script>
 <style>
-    .register-btn {
-    }
 </style>
