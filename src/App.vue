677b7577503757cdf78028e7d564092fd393e4c5
VIVIFY <vivify@MacBook-Pro.local>
2020-01-23 16:30:41 +0100

    #LVNA-15 Remove redundant code

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,12 @@
   <router-view></router-view>
 </template>
 <script>
-import { router } from "./main";
 export default {
   name: "app",
 
-  mounted() {
+  created() {
     if (!localStorage.token || window.location.pathname !== "/register") {
-      router.push({ name: "login" });
+      this.$router.push({ name: "login" });
     }
   }
 };
